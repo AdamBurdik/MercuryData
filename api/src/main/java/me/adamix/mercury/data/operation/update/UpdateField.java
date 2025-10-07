9b63fd3dadec448b725da1d38920eb3db8e7ee7a
AdamIx <adamburdik9@gmail.com>
2025-10-07 16:47:39 +0200

    Add: annotations to update field function

--- api/src/main/java/me/adamix/mercury/data/operation/update/UpdateField.java
+++ api/src/main/java/me/adamix/mercury/data/operation/update/UpdateField.java
@@ -3,10 +3,11 @@ package me.adamix.mercury.data.operation.update;
 import me.adamix.mercury.data.codec.Codec;
 import me.adamix.mercury.data.key.Key;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.function.Function;
 
-public record UpdateField<T>(@NotNull Key key, @NotNull Codec<T> codec, @NotNull Function<T, T> function, boolean isConstant) {
+public record UpdateField<T>(@NotNull Key key, @NotNull Codec<T> codec, @NotNull Function<@Nullable T, @NotNull T> function, boolean isConstant) {
 	public static <T> @NotNull UpdateField<T> of(@NotNull Key key, @NotNull Codec<T> codec, @NotNull T value) {
 		return new UpdateField<>(key, codec, v -> value, true);
 	}
