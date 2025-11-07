1fee4c5117721347e6920f4ce22cc481bd33fa9a
Adam Burdík <adamburdik9@gmail.com>
2025-11-07 18:23:38 +0100

    Add: record scope interface

---
+++ api/src/main/java/me/adamix/mercury/data/scope/RecordScope.java
@@ -0,0 +1,51 @@
+package me.adamix.mercury.data.scope;
+
+import com.google.gson.JsonElement;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collection;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+
+public interface RecordScope {
+
+	// SET FIELD
+	@NotNull
+	RecordScope setFieldSync(@NotNull String key, @NotNull JsonElement value);
+	default @NotNull CompletableFuture<@NotNull RecordScope> setField(@NotNull String key, @NotNull JsonElement value) {
+		return CompletableFuture.supplyAsync(() -> setFieldSync(key, value));
+	}
+
+	// GET FIELD
+	@NotNull
+	Optional<JsonElement> getFieldSync(@NotNull String key);
+	default @NotNull CompletableFuture<Optional<JsonElement>> getField(@NotNull String key) {
+		return CompletableFuture.supplyAsync(() -> getFieldSync(key));
+	}
+
+	// REMOVE FIELD
+	@NotNull
+	RecordScope removeFieldSync(@NotNull String key);
+	default @NotNull CompletableFuture<@NotNull RecordScope> removeField(@NotNull String key) {
+		return CompletableFuture.supplyAsync(() -> removeFieldSync(key));
+	}
+
+	// CHECK IF FIELD EXISTS
+	boolean fieldExistsSync(@NotNull String key);
+	default @NotNull CompletableFuture<Boolean> fieldExists(@NotNull String key) {
+		return CompletableFuture.supplyAsync(() -> fieldExistsSync(key));
+	}
+
+	// CLEAR RECORD
+	@NotNull RecordScope clearSync();
+	default @NotNull CompletableFuture<@NotNull RecordScope> clear() {
+		return CompletableFuture.supplyAsync(this::clearSync);
+	}
+
+	// LIST FIELDS
+	@NotNull
+	Collection<String> listFieldsSync();
+	default @NotNull CompletableFuture<@NotNull Collection<String>> listFields() {
+		return CompletableFuture.supplyAsync(this::listFieldsSync);
+	}
+}
